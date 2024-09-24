5a89cdc10a575a2af36c576b5000309e9d18ed13
chynasan <chsander@redhat.com>
2024-09-23 19:47:14 -0500

    Broke and fixed the database, adjusted the model so that the Role option in hero list shows the name of the role as well

--- overwatch/overwatch_api/models.py
+++ overwatch/overwatch_api/models.py
@@ -3,20 +3,21 @@ from django.db import models
 
 # Create your models here.
 
+class Role(models.Model):
+    Name = models.CharField(max_length=10, primary_key=True)
+    PassiveAbility = models.CharField(max_length=250)
+
 class Hero(models.Model):
-    Name = models.CharField(max_length=30)
+    Name = models.CharField(max_length=30, primary_key=True)
     Role = models.ForeignKey(
-        'Role', 
+        Role, 
+        related_name="Role",
         on_delete=models.DO_NOTHING,
     )
     Description = models.CharField(max_length=500)
     Abilities = models.CharField(max_length=500)
     Health = models.IntegerField()
     Birthday = models.DateField()
-
-class Role(models.Model):
-    Name = models.CharField(max_length=10)
-    PassiveAbility = models.CharField(max_length=250)
 
 #TODO: Update this to include a field that has preset selectable options for hybrid, control, etc as a type field
 #list for map choices
